c153e6f5208a70b4cdd583018acd1cf20784e68f
ersaurabhtiwari <ersaur200185@gmail.com>
2022-07-29 09:52:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [date,setDate]=useState("");
+  const [amount,setAmount]=useState("");
+  const [payment,setPayment]=useState("");
+  const [remark,setRemark]=useState("");
+  const[list,setList]=useState([]);
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log(date,amount,payment,remark);
+    const data={date,amount,payment,remark}
+    if(date&&amount&&payment&&remark){
+      setList((ls)=>[...ls,data])
+      setDate("")
+      setAmount("")
+      setPayment("")
+      setRemark("")
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <form onSubmit={handleSubmit}>
+      <h1>Receipt Name</h1>
+     <div className="container">
+      Date:<input type="text" name="date" value={date} onChange={(e)=>setDate(e.target.value)} id="date"  placeholder='Enter Date'/><br />
+      Amount: <input type="text" name="amount" value={amount} onChange={(e)=>setAmount(e.target.value)} id="amount"  placeholder='Enter Amount(in INR)'/><br />
+      Payment Mode: <input type="text" name="payment" value={payment} onChange={(e)=>setPayment(e.target.value)} id="payment"  placeholder='Cash, Debit card , Credit card'/><br />
+      Remark: <input type="text" name="remark" value={remark} onChange={(e)=>setRemark(e.target.value)} id="remark"  placeholder='Enter remark'/><br />
+      <button id='cancel'>CANCEL</button>
+      <button id='submit'>SUBMIT</button>
+     </div>
+    </form>
+
+
+    {
+      list.map((a)=><div className='finallist'>
+        <li>{a.date}</li>
+        <li>{a.amount}</li>
+        <li>{a.payment}</li>
+        <li>{a.remark}</li>
+      </div>
+
+      )
+    }
+    </>
   );
 }
 
